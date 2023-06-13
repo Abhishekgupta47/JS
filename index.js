efc76b47e7249e0a8ac0ac8eb7419d927dc80aa8
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-06-13 12:04:05 +0530

    time and date model complete

---
+++ index.js
@@ -0,0 +1,50 @@
+// console.log('hello world');
+
+// --------DATE OBJECT------------
+
+// let currDate = new Date();
+// console.log(currDate);
+
+// var d = new Date (2023,0,5,10,33,30,0);
+// console.log(d.toLocaleString());
+
+// var d = new Date(1609574531435);
+// console.log(d.toLocaleString());
+
+// const curDate = new Date();
+// how tto vet the endivisual date
+
+// console.log(curDate.toLocaleString());
+// console.log(curDate.getFullYear());
+// console.log(curDate.getMonth());
+// console.log(curDate.getDate());
+// console.log(curDate.getDay());
+
+
+
+
+// ---------TimeMethod-----------
+
+// how to get the indivisual current time 
+
+
+// const curTime = new Date();
+
+// console.log(curTime.getTime());
+// // the getTime() method returns the number of miliseconds since jan 1 ,1970
+// console.log(curTime.getHours());
+// //  the getHours() methods returns the hours of a date as a number(0-9)
+// console.log(curTime.getMinutes());
+// console.log(curTime.getSeconds());
+// console.log(curTime.getMilliseconds());
+
+
+// ------how to set the indivisual Time
+
+let curTime = new Date();
+
+// console.log(curTime.setTime());
+console.log(curTime.setHours(5));
+console.log(curTime.setMinutes(5));
+console.log(curTime.setSeconds(5));
+console.log(curTime.setMilliseconds(5));
